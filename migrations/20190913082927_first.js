a5fb10f9119f66d6bc91a84e53db85ed7deb4d2a
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-13 08:49:11 -0700

    created table in migrations, has not been migrated yet

---
+++ migrations/20190913082927_first.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('project_name', 255).notNullable();
+      tbl.string('project_description', 1000);
+      tbl.boolean('completed')
+  })
+  .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.string('task_description', 1000).unique().notNullable();
+      tbl.string('notes');
+      tbl.boolean('completed');
+      tbl.integer('project_id')
+      .unsigned().notNullable().reference('id').inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+  .createTable('resources', tbl => {
+      tbl.increments();
+      tbl.string('resource_name', 500).notNullable();
+      tbl.string('resource_description', 1000);
+      tbl.integer('project_id')
+      .unsigned().notNullable().reference('id').inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('resources')
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('projects')
+};
